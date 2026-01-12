1ef14eda436f4a3585d0bb74b8484c7f3cfedda5
Valiantsin Dzerakh <valentderah@gmail.com>
2026-01-12 22:36:36 +0300

    image cropping

--- MediaManager/platforms/windows/Imaging/ImageUtils.cs
+++ MediaManager/platforms/windows/Imaging/ImageUtils.cs
@@ -1,12 +1,13 @@
 using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace MediaManager.Windows.Imaging;
 
@@ -34,52 +35,42 @@ static class ImageUtils
         return Convert.ToBase64String(outputBuffer.ToArray());
     }
 
+    public static byte[] CropToSquare(byte[] sourcePixelBytes, uint width, uint height, int targetSize)
+    {
+        var image = Image.LoadPixelData<Rgba32>(sourcePixelBytes, (int)width, (int)height);
+        image.Mutate(x => x.Resize(new ResizeOptions
+        {
+            Size = new Size(targetSize, targetSize),
+            Mode = ResizeMode.Crop
+        }));
+
+        var croppedPixels = new byte[targetSize * targetSize * 4];
+        image.CopyPixelDataTo(croppedPixels);
+        return croppedPixels;
+    }
+
     public static async Task<string> ConvertIconToBase64Async(string exePath, int size)
     {
-        using var icon = Icon.ExtractAssociatedIcon(exePath);
-        if (icon == null)
+        using var iconStream = new MemoryStream();
+        using (var icon = System.Drawing.Icon.ExtractAssociatedIcon(exePath))
         {
-            return string.Empty;
-        }
-
-        using var bitmap = new Bitmap(icon.ToBitmap(), size, size);
-        var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-            ImageLockMode.ReadOnly,
-            PixelFormat.Format32bppArgb);
-
-        try
-        {
-            var width = bitmap.Width;
-            var height = bitmap.Height;
-            var stride = bitmapData.Stride;
-
-            var bgraBytes = new byte[stride * height];
-            Marshal.Copy(bitmapData.Scan0, bgraBytes, 0, bgraBytes.Length);
-
-            var rgbaBytes = new byte[width * height * 4];
-
-            for (int y = 0; y < height; y++)
+            if (icon == null)
             {
-                int srcRowOffset = y * stride;
-                int dstRowOffset = y * width * 4;
-
-                for (int x = 0; x < width; x++)
-                {
-                    int srcIndex = srcRowOffset + (x * 4);
-                    int dstIndex = dstRowOffset + (x * 4);
-
-                    rgbaBytes[dstIndex] = bgraBytes[srcIndex + 2];
-                    rgbaBytes[dstIndex + 1] = bgraBytes[srcIndex + 1];
-                    rgbaBytes[dstIndex + 2] = bgraBytes[srcIndex];
-                    rgbaBytes[dstIndex + 3] = bgraBytes[srcIndex + 3];
-                }
+                return string.Empty;
             }
+            icon.ToBitmap().Save(iconStream, System.Drawing.Imaging.ImageFormat.Png);
+        }
+        iconStream.Seek(0, SeekOrigin.Begin);
 
-            return await EncodeImageToBase64Async(rgbaBytes, width);
-        }
-        finally
+        using var image = await Image.LoadAsync<Rgba32>(iconStream);
+        image.Mutate(x => x.Resize(new ResizeOptions
         {
-            bitmap.UnlockBits(bitmapData);
-        }
+            Size = new Size(size, size),
+            Mode = ResizeMode.Crop
+        }));
+
+        var rgbaBytes = new byte[size * size * 4];
+        image.CopyPixelDataTo(rgbaBytes);
+        return await EncodeImageToBase64Async(rgbaBytes, size);
     }
 }
--- MediaManager/platforms/windows/Imaging/ThumbnailProcessor.cs
+++ MediaManager/platforms/windows/Imaging/ThumbnailProcessor.cs
@@ -19,19 +19,7 @@ static class ThumbnailProcessor
             using var thumbnailStream = await thumbnail.OpenReadAsync();
             var decoder = await BitmapDecoder.CreateAsync(thumbnailStream);
 
-            var (scaledWidth, scaledHeight, offsetX, offsetY) = CalculateScaleAndOffset(
-                decoder.PixelWidth,
-                decoder.PixelHeight,
-                TargetSize
-            );
-
-            var transform = new BitmapTransform
-            {
-                ScaledWidth = scaledWidth,
-                ScaledHeight = scaledHeight,
-                InterpolationMode = BitmapInterpolationMode.Linear
-            };
-
+            var transform = new BitmapTransform();
             var pixelData = await decoder.GetPixelDataAsync(
                 BitmapPixelFormat.Rgba8,
                 BitmapAlphaMode.Premultiplied,
@@ -40,8 +28,14 @@ static class ThumbnailProcessor
                 ColorManagementMode.ColorManageToSRgb
             );
 
-            var scaledPixelBytes = pixelData.DetachPixelData();
-            var finalPixels = CreateCenteredImage(scaledPixelBytes, scaledWidth, scaledHeight, TargetSize, offsetX, offsetY);
+            var sourcePixelBytes = pixelData.DetachPixelData();
+
+            var finalPixels = ImageUtils.CropToSquare(
+                sourcePixelBytes,
+                decoder.PixelWidth,
+                decoder.PixelHeight,
+                TargetSize
+            );
 
             info.CoverArtBase64 = await ImageUtils.EncodeImageToBase64Async(finalPixels, TargetSize);
 
@@ -59,71 +53,6 @@ static class ThumbnailProcessor
         }
     }
 
-    private static (uint scaledWidth, uint scaledHeight, int offsetX, int offsetY) CalculateScaleAndOffset(
-        uint originalWidth,
-        uint originalHeight,
-        int targetSize)
-    {
-        var aspectRatio = (double)originalWidth / originalHeight;
-        uint scaledWidth, scaledHeight;
-        int offsetX = 0;
-        int offsetY = 0;
-
-        if (aspectRatio > 1.0)
-        {
-            scaledWidth = (uint)targetSize;
-            scaledHeight = (uint)Math.Round(targetSize / aspectRatio);
-        }
-        else
-        {
-            scaledHeight = (uint)targetSize;
-            scaledWidth = (uint)Math.Round(targetSize * aspectRatio);
-            offsetX = (targetSize - (int)scaledWidth) / 2;
-        }
-
-        return (scaledWidth, scaledHeight, offsetX, offsetY);
-    }
-
-    private static byte[] CreateCenteredImage(
-        byte[] scaledPixels,
-        uint scaledWidth,
-        uint scaledHeight,
-        int targetSize,
-        int offsetX,
-        int offsetY)
-    {
-        var finalPixels = new byte[targetSize * targetSize * 4];
-
-        for (int y = 0; y < targetSize; y++)
-        {
-            for (int x = 0; x < targetSize; x++)
-            {
-                var targetIndex = (y * targetSize + x) * 4;
-
-                if (x >= offsetX && x < offsetX + scaledWidth &&
-                    y >= offsetY && y < offsetY + scaledHeight)
-                {
-                    var sourceX = x - offsetX;
-                    var sourceY = y - offsetY;
-                    var sourceIndex = (sourceY * (int)scaledWidth + sourceX) * 4;
-
-                    finalPixels[targetIndex] = scaledPixels[sourceIndex];
-                    finalPixels[targetIndex + 1] = scaledPixels[sourceIndex + 1];
-                    finalPixels[targetIndex + 2] = scaledPixels[sourceIndex + 2];
-                    finalPixels[targetIndex + 3] = scaledPixels[sourceIndex + 3];
-                }
-                else
-                {
-                    finalPixels[targetIndex] = 0;
-                    finalPixels[targetIndex + 1] = 0;
-                    finalPixels[targetIndex + 2] = 0;
-                    finalPixels[targetIndex + 3] = 255;
-                }
-            }
-        }
-
-        return finalPixels;
-    }
 
     private static async Task<List<byte[]>> SplitImageIntoPartsAsync(byte[] sourcePixels, int sourceSize)
     {
